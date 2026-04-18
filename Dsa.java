984d04dc69c6193bf1e3a59f98249d6f09791fa1
Shubham <64083206+sk244194@users.noreply.github.com>
2026-04-18 23:53:29 +0530

    DSA Started:

---
+++ Dsa.java
@@ -0,0 +1,19 @@
+public class Dsa {
+    public static void main(String[] args) {
+        
+
+        // Leetcode 3783
+
+        // String str = String.valueOf(n);
+        // To convert integer into string
+
+        // String reversed = new StringBuilder(str).reverse().toString();
+        // Reversing the string
+
+        // int num = Integer.valueOf(reversed);
+        // String to integer
+
+        // return Math.abs(num - n);
+    }
+    
+}
